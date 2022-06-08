ea9b58f3a76dec290d0523aef47b71e1cbffc273
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-06-08 18:05:43 +0000

    ajout enfant et conjoint

--- resources/views/conjoint/create.blade.php
+++ resources/views/conjoint/create.blade.php
@@ -15,22 +15,40 @@
                                 <div class="col">
                                     <div class="form-group">
                                         <label for="">Nom</label>
-                                        <input type="text" name="nom" id="" class="form-control" placeholder=""
+                                        <input type="text" name="nom" id=""
+                                            class="form-control @error('nom') is-invalid @enderror " placeholder=""
                                             aria-describedby="helpId">
+                                        @error('nom')
+                                            <small id="helpId" class="form-text text-muted">
+                                                {{ $errors->first('nom') }}
+                                            </small>
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="col">
                                     <div class="form-group">
                                         <label for="">Prénom</label>
-                                        <input type="text" name="prenom" id="" class="form-control" placeholder=""
+                                        <input type="text" name="prenom" id=""
+                                            class="form-control @error('prenom') is-invalid @enderror " placeholder=""
                                             aria-describedby="helpId">
+                                        @error('prenom')
+                                            <small id="helpId" class="form-text text-muted">
+                                                {{ $errors->first('prenom') }}
+                                            </small>
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="col">
                                     <div class="form-group">
                                         <label for="">Date de naissance</label>
-                                        <input type="date" name="date_naissance" id="" class="form-control" placeholder=""
+                                        <input type="date" name="date_naissance" id=""
+                                            class="form-control @error('date_naissance')  @enderror " placeholder=""
                                             aria-describedby="helpId">
+                                        @error('date_naissance')
+                                            <small id="helpId" class="form-text text-muted">
+                                                {{ $errors->first('date_naissance') }}
+                                            </small>
+                                        @enderror
                                     </div>
                                 </div>
                             </div>
@@ -45,8 +63,14 @@
                                 <div class="col">
                                     <div class="form-group">
                                         <label for="">Date mariage</label>
-                                        <input type="date" name="date_mariage" id="" class="form-control" placeholder=""
+                                        <input type="date" name="date_mariage" id=""
+                                            class="form-control @error('date_mariage') is-invalid @enderror " placeholder=""
                                             aria-describedby="helpId">
+                                        @error('date_mariage')
+                                            <small id="helpId" class="form-text text-muted">
+                                                {{ $errors->first('date_mariage') }}
+                                            </small>
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="col">
